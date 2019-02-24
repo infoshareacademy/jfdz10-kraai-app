b0ae2c6c7c7e0d43c7c8ffaa20f454766b1d29af
Daria Kozielecka <dkozielecka@MacBook-Air-Daria.local>
2019-02-24 19:17:41 +0100

    create nav

--- src/components/Navigation/Nav.js
+++ src/components/Navigation/Nav.js
@@ -1,5 +1,46 @@
-import React from "react";
+import React, { Component } from "react";
+import { Router } from "react-router-dom";
+import { Menu } from "semantic-ui-react";
+import Logo from "../../img/logo.png";
 
-const Nav = () => {
-  return <ul>import {} from 'module';</ul>;
-};
+export default class Nav extends Component {
+  state = {};
+
+  handleItemClick = (event, { name }) => this.setState({ activeItem: name });
+
+  render() {
+    const { activeItem } = this.state;
+
+    return (
+      <Menu stackable>
+        <Menu.Item>
+          <img src={Logo} alt="Logo" />
+        </Menu.Item>
+
+        <Menu.Item
+          name="features"
+          active={activeItem === "features"}
+          onClick={this.handleItemClick}
+        >
+          Features
+        </Menu.Item>
+
+        <Menu.Item
+          name="testimonials"
+          active={activeItem === "testimonials"}
+          onClick={this.handleItemClick}
+        >
+          Testimonials
+        </Menu.Item>
+
+        <Menu.Item
+          name="sign-in"
+          active={activeItem === "sign-in"}
+          onClick={this.handleItemClick}
+        >
+          Sign-in
+        </Menu.Item>
+      </Menu>
+    );
+  }
+}
