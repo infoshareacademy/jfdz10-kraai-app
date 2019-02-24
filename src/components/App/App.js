c72c08b9de09047056b04179181508f4ce855a89
Daria Kozielecka <dkozielecka@MacBook-Air-Daria.local>
2019-02-24 20:29:38 +0100

    nav done

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from "react";
 import "./App.css";
 import Nav from "../Navigation/Nav.js";
+import { Route } from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
       <div className="wrapper">
         <Nav />
+        <Route exact path="/szukaj" component={() => <h1>szukaj</h1>} />
+        <Route exact path="/tinder-mode" component={() => <h1>tinder</h1>} />
+        <Route exact path="/profil" component={() => <h1>profil</h1>} />
       </div>
     );
   }
--- src/components/Navigation/Nav.js
+++ src/components/Navigation/Nav.js
@@ -1,58 +1,29 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Link, Route } from "react-router-dom";
+import { BrowserRouter as Router, NavLink, Route } from "react-router-dom";
 import { Menu } from "semantic-ui-react";
 import Logo from "../../img/logo.png";
 import Test from "./Test.js";
 
-export default class Nav extends Component {
-  state = {};
+const Nav = () => {
+  return (
+    <Menu stackable>
+      <NavLink className="item" exact to="/">
+        <img src={Logo} alt="Logo" />
+      </NavLink>
 
-  handleItemClick = (event, { name }) => this.setState({ activeItem: name });
+      <NavLink className="item" to="/szukaj" name="features">
+        Szukaj
+      </NavLink>
 
-  render() {
-    const { activeItem } = this.state;
+      <NavLink className="item" to="/tinder-mode" name="tinder-mode">
+        Tinder mode
+      </NavLink>
 
-    return (
-      <Router>
-        <Menu stackable>
-          <Link className="item" to="/szukaj">
-            <img src={Logo} alt="Logo" />
-          </Link>
+      <NavLink className="item" to="/profil" name="profil">
+        Profil
+      </NavLink>
+    </Menu>
+  );
+};
 
-          <Link
-            className="item"
-            to="/szukaj"
-            name="features"
-            active={activeItem === "features"}
-            onClick={this.handleItemClick}
-          >
-            Szukaj
-          </Link>
-
-          <Link
-            className="item"
-            to="/szukaj"
-            name="testimonials"
-            active={activeItem === "testimonials"}
-            onClick={this.handleItemClick}
-          >
-            Tinder mode
-          </Link>
-
-          <Link
-            className="item"
-            to="/szukaj"
-            name="sign-in"
-            active={activeItem === "sign-in"}
-            onClick={this.handleItemClick}
-          >
-            Profil
-          </Link>
-          <Route exact path="/szukaj" component={Test} />
-          <Route path="/tinder-mode" component={Test} />
-          <Route path="/zaloguj" component={Test} />
-        </Menu>
-      </Router>
-    );
-  }
-}
+export default Nav;
