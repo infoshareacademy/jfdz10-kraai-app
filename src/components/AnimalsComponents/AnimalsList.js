c79be3cc8bd54f2fb288052f1c31b2912979d60e
lbillot <43831212+lbillot@users.noreply.github.com>
2019-03-24 16:17:57 +0100

    Revert "Fz10 kr 51"

--- src/components/AnimalsComponents/AnimalsList.js
+++ src/components/AnimalsComponents/AnimalsList.js
@@ -1,11 +1,13 @@
-import React, { Component, Fragment } from "react";
-import { Card, Image } from "semantic-ui-react";
-import { NavLink } from "react-router-dom";
 
-import AnimalsFilter from "./AnimalsFilter";
+import React, { Component, Fragment } from "react";
+import { Card, Image, Icon } from "semantic-ui-react";
 
 const animals = () =>
   fetch(process.env.PUBLIC_URL + "/animals.json").then(response =>
+    response.json()
+  );
+const user = () =>
+  fetch(process.env.PUBLIC_URL + "/user.json").then(response =>
     response.json()
   );
 
@@ -13,46 +15,74 @@ class AnimalsList extends Component {
   state = {
     loading: true,
     animals: [],
-    filter: {
-      name: "",
-      kind: "",
-    }
+    user: {
+      favAnimalId: []
+    },
+    userFavoriteAnimals: []
   };
 
   componentDidMount() {
     animals().then(animals => this.setState({ animals: animals }));
+    user().then(user => this.setState({ user }));
+    if(localStorage.getItem('userFav')){
+      this.setState({userFavoriteAnimals: JSON.parse(localStorage.getItem('userFav'))})
+    }
+
   }
-  getFilteredAnimals() {
-    return this.state.animals.filter(animal => {
-      const animalNameLowercase = animal.name.toLowerCase();
-      const nameFilteredLowercase = this.state.filter.name.toLowerCase();
-      const animalKind = animal.kindId
-      const kindFilter = this.state.filter.kind;
-      console.log(animalKind);
-      return (
-        animalNameLowercase.includes(nameFilteredLowercase) &&
-        animalKind === kindFilter
-      );
-    });
+  componentWillUnmount() {
+    localStorage.setItem("userFav", JSON.stringify(this.state.userFavoriteAnimals));
   }
 
   render() {
-    const { animals } = this.state;
+    const { animals, user , userFavoriteAnimals} = this.state;
     return (
       <Fragment>
-        <AnimalsFilter onFilterChange={filter => this.setState({ filter })} />
         <Card.Group doubling itemsPerRow={3} stackable>
-          {this.getFilteredAnimals().map(animal => (
+          {animals.map(animal => (
             <Card key={animal.id}>
-              <NavLink to={`animals/${animal.id}`}>
-                <Image style={{ textAlign: "center" }} src={animal.avatar} />
-              </NavLink>
-
+              
+                <Image src={animal.avatar} height ='300px'/>
+              
               <Card.Content>
-                <Fragment>
-                  <Card.Header>{animal.name}</Card.Header>
-                  <Card.Description>{animal.description}</Card.Description>
-                </Fragment>
+                
+                  <Fragment>
+                    <Card.Header>{animal.name}</Card.Header>
+                    <Card.Meta>{animal.description}</Card.Meta>
+                    <Card.Description>
+                      Aktualnie przebywa w {animal.shelterId}
+                    </Card.Description>
+                    {!userFavoriteAnimals.some(
+                      favAnimal => favAnimal === animal.id
+                    ) ? (
+                      <Icon
+                        name="heart outline"
+                        color="black"
+                        size='big'
+                        onClick={e =>
+                          this.setState(
+                            ({userFavoriteAnimals:[
+                              ...userFavoriteAnimals,
+                              animal.id
+                            ]})
+                          )
+                        }
+                      />
+                    ) : (
+                      <Icon
+                        name="heart"
+                        color="red"
+                        size='big'
+                        onClick={e =>
+                          this.setState(
+                            ({userFavoriteAnimals: userFavoriteAnimals.filter(
+                              id => id !== animal.id
+                            )})
+                          )
+                        }
+                      />
+                    )}
+                  </Fragment>
+                
               </Card.Content>
             </Card>
           ))}
