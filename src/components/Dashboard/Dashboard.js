98576497594cf896b011119f96f2f517f365ec1a
Daria Kozielecka <dkozielecka@macbook-air-daria-1.home>
2019-05-25 23:01:39 +0200

    add bg dashboard

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -8,18 +8,122 @@ import Particles from "react-particles-js";
 const Dashboard = () => {
     return (
         <div className="dash__wrapper">
-            <Particles
-                params={{
-                    polygon: {
-                        type: "outside",
-                        move: {
-                            radius: 10
-                        }
-                    }
-                }}
-            />
-            <div className="content">
-                <h1>dupa</h1>
+            <div className="particles__wrapper">
+                <Particles
+                    params={{
+                        particles: {
+                            number: {
+                                value: 80,
+                                density: {
+                                    enable: true,
+                                    value_area: 800
+                                }
+                            },
+                            color: {
+                                value: "#ffffff"
+                            },
+                            shape: {
+                                type: "triangle",
+                                stroke: {
+                                    width: 0,
+                                    color: "#000000"
+                                },
+                                polygon: {
+                                    nb_sides: 9
+                                },
+                                image: {
+                                    src: "img/github.svg",
+                                    width: 100,
+                                    height: 100
+                                }
+                            },
+                            opacity: {
+                                value: 0.5,
+                                random: false,
+                                anim: {
+                                    enable: false,
+                                    speed: 1,
+                                    opacity_min: 0.1,
+                                    sync: false
+                                }
+                            },
+                            size: {
+                                value: 1,
+                                random: true,
+                                anim: {
+                                    enable: true,
+                                    speed: 34.107242916656496,
+                                    size_min: 7.308694910712106,
+                                    sync: false
+                                }
+                            },
+                            line_linked: {
+                                enable: true,
+                                distance: 150,
+                                color: "#ffffff",
+                                opacity: 0.4,
+                                width: 1
+                            },
+                            move: {
+                                enable: true,
+                                speed: 6,
+                                direction: "none",
+                                random: true,
+                                straight: false,
+                                out_mode: "out",
+                                bounce: false,
+                                attract: {
+                                    enable: false,
+                                    rotateX: 600,
+                                    rotateY: 1200
+                                }
+                            }
+                        },
+                        interactivity: {
+                            detect_on: "canvas",
+                            events: {
+                                onhover: {
+                                    enable: true,
+                                    mode: "repulse"
+                                },
+                                onclick: {
+                                    enable: true,
+                                    mode: "push"
+                                },
+                                resize: true
+                            },
+                            modes: {
+                                grab: {
+                                    distance: 400,
+                                    line_linked: {
+                                        opacity: 1
+                                    }
+                                },
+                                bubble: {
+                                    distance: 400,
+                                    size: 40,
+                                    duration: 2,
+                                    opacity: 8,
+                                    speed: 3
+                                },
+                                repulse: {
+                                    distance: 200,
+                                    duration: 0.4
+                                },
+                                push: {
+                                    particles_nb: 4
+                                },
+                                remove: {
+                                    particles_nb: 2
+                                }
+                            }
+                        },
+                        retina_detect: true
+                    }}
+                />
+            </div>
+            <div className="tittle">
+                <h1>Paw Love</h1>
             </div>
         </div>
     );
